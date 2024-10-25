4038c14cea518720a3184f3379b09870dc0fc804
Camilo Mora <camilomorag2018@gmail.com>
2024-10-25 11:15:14 -0500

    Metodo sumar y restar

--- src/main/java/com/miempresa/app/Calculadora.java
+++ src/main/java/com/miempresa/app/Calculadora.java
@@ -2,4 +2,11 @@ package main.java.com.miempresa.app;
 
 public class Calculadora {
 
+		public static double sumar(double a , double b) {
+			return a+b;
+		}
+		
+		public static double restar(double a , double b) {
+			return a-b;
+		}
 }
